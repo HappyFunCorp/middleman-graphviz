6270dcf4238f135c5dc48a1bc60ec2343a83a2df
Bill Christian Tyros <me@btyros.com>
2020-08-17 22:28:27 -0400

    Update extension to work with Middleman 4.2 due to changes in the Middleman API

--- lib/middleman/graphviz/extension.rb
+++ lib/middleman/graphviz/extension.rb
@@ -1,13 +1,25 @@
-require 'middleman/graphviz/helpers'
-
 module Middleman
   module Graphviz
 
     class Extension < Middleman::Extension
       def initialize( app, options_hash = {}, &block)
         super
+      end
+      helpers do
+        def graphviz( &block )
+          if block_given?
+            data = capture_html(&block)
 
-        app.helpers Middleman::Graphviz::Helpers
+            out, err, status = Open3.capture3( "dot -Tsvg", stdin_data: data )
+
+            # puts "Status = #{status}"
+            # puts err
+
+            svg = out.gsub( /.*<svg/m, "<svg" ).gsub( /\n/m, "")
+
+            concat_content(svg.html_safe)
+          end
+        end
       end
     end
   end
